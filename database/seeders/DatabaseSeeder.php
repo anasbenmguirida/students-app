1276477d9c85490c13e92fc5eba4b4542d5e8a81
anasbenmguirida <anasbenmki@gmail.com>
2024-07-16 15:55:56 +0100

    some modifications

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            enseignantSeeder::class,
+            ensmatSeeder::class,
+            MatiereSeeder::class,
+            UsersTableSeeder::class,
+            YourTableSeederSeeder::class,
+            // Add other seeders here
+        ]);
     }
 }
